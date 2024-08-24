0549a0025d51e9a6f45304fad6c933442e21ee60
Imran shaikh <imranpathan7040@gmail.com>
2024-08-24 18:56:49 +0530

    element added

--- src/components/App.js
+++ src/components/App.js
@@ -5,32 +5,45 @@ import './../styles/App.css';
 const App = () => {
   const [result,setResult] = useState([]);
   const [input,setInput] = useState("");
+  const [error,setError] = useState("");
   const apiKey = "99eb9fd1";
 
   const handleSearch = ()=>{
-    const apiUrl = `https://www.omdbapi.com/?apikey=${apiKey}&s=${input}`;
-   fetch(apiUrl)
-   .then((res)=>res.json())
-   .then((data)=>{
-      console.log(data.Search);
-    setResult(data.Search)})
-    
+    try {
+      
+      const apiUrl = `https://www.omdbapi.com/?apikey=${apiKey}&s=${input}`;
+     fetch(apiUrl)
+     .then((res)=>res.json())
+     .then((data)=>{
+        console.log(data.Search);
+      setResult(data.Search)})
+      
+    } catch (error) {
+        console.log(error);
+        setError(error);
+    }
 
   }
 
   return (
     <div>
       <p>Search Movie</p>
+      <form>
+
       <input type="text" value={input} onChange={e=>setInput(e.target.value)}/>
       <button onClick={handleSearch}>Search</button>
+      </form>
+      <ul>
+      {error && <div className="error">{error}</div>}
       {
         result && result.map((mov,index)=>(
-          <div key={index}>
-            <h1>{mov.title}</h1>
+          <li key={index}>
+            <h1>{mov.Title}</h1>
             <img src={mov.Poster} alt={mov.title}/>
-          </div>
+          </li>
         ))
       }
+      </ul>
     </div>
   )
 }
